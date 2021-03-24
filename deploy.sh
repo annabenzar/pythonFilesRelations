eccf8083ed9fd0dd0a939a98f6261c85873dff41
unknown <benzaranna@gmail.com>
2021-03-24 19:50:15 +0200

    Final

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-mvn clean install
-cp target\ces-python-files-relations.jar C:\Users\Andrada\.dx-platform\plugins\python-relations
-cp plugin-info.json C:\Users\Andrada\.dx-platform\plugins\python-relations
